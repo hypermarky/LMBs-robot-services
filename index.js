edc75f8791a69c2d12bef2fb70e23ac967ee6fb8
hypermarky <166612441+hypermarky@users.noreply.github.com>
2025-05-09 14:33:27 +0200

    testing

---
+++ index.js
@@ -0,0 +1,46 @@
+import dotenv from 'dotenv'
+dotenv.config()
+
+import { 
+    Client, 
+    GatewayIntentBits,
+    ButtonBuilder,
+    ButtonStyle,
+    ModalBuilder,
+    TextInputBuilder,
+    TextInputStyle, 
+} from 'discord.js';
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.GuildMembers,
+        GatewayIntentBits.DirectMessages,
+    ],
+});
+
+client.login(process.env.DISCORD_TOKEN)
+
+const btn = new ButtonBuilder()
+        .setCustomId('test')
+        .setLabel('Do you want to test this?')
+        .setStyle(ButtonStyle.Primary);
+
+client.on("messageCreate", async (message) => {
+
+    console.log(message)
+
+    if (!message?.author.bot) {
+        message.author.send({
+            content: 'Push my btns!',
+            components: [btn]
+        });
+    }
+});
+
+client.on('interactionCreate', async (interaction) => {
+    if (interaction.customId === 'test') {
+        await interaction.reply('You tested my button bruh!!!!!')
+    }
+});
