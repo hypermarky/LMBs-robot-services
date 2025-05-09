efabf70345c2b849a84fa7190f205d702466f5aa
hypermarky <166612441+hypermarky@users.noreply.github.com>
2025-05-09 14:48:38 +0200

    update to fix errors hopefully

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 import dotenv from 'dotenv'
 dotenv.config()
 
-import { 
-    Client, 
+import {
+    Client,
     GatewayIntentBits,
+    ActionRowBuilder,
     ButtonBuilder,
     ButtonStyle,
     ModalBuilder,
@@ -17,30 +18,53 @@ const client = new Client({
         GatewayIntentBits.GuildMessages,
         GatewayIntentBits.GuildMembers,
         GatewayIntentBits.DirectMessages,
+        GatewayIntentBits.MessageContent,
     ],
 });
 
-client.login(process.env.DISCORD_TOKEN)
+client.login(process.env.DISCORD_TOKEN);
 
 const btn = new ButtonBuilder()
-        .setCustomId('test')
-        .setLabel('Do you want to test this?')
-        .setStyle(ButtonStyle.Primary);
+    .setCustomId('test')
+    .setLabel('Do you want to test this?')
+    .setStyle(ButtonStyle.Primary);
+
+const row = new ActionRowBuilder()
+    .addComponents(btn);
 
 client.on("messageCreate", async (message) => {
 
-    console.log(message)
 
     if (!message?.author.bot) {
-        message.author.send({
-            content: 'Push my btns!',
-            components: [btn]
-        });
+        try {
+            await message.author.send({
+                content: 'Push my btns!',
+                components: [row] 
+            });
+        } catch (error) {
+            console.error(`Could not send DM to ${message.author.tag} (ID: ${message.author.id}). Error:`, error.message);
+
+            if (error.code === 50007) { 
+                message.reply("I tried to send you a DM, but it seems your DMs are closed. Please enable DMs from server members to use this feature.").catch(console.error);
+            }
+        }
     }
 });
 
 client.on('interactionCreate', async (interaction) => {
+    if (!interaction.isButton()) return; // Make sure it's a button interaction
+
     if (interaction.customId === 'test') {
-        await interaction.reply('You tested my button bruh!!!!!')
+        try {
+            await interaction.reply('You tested my button bruh!!!!!');
+        } catch (error) {
+            console.error("Error replying to button interaction:", error);
+        }
     }
 });
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('error', console.error);
